b62d8cab357c4907e51fbdaff18ee1a95dc0e970
farhan.yudha2016we@gmail.com <farhan.yudha2016we@gmail.com>
2022-07-15 15:29:26 +0700

    Up

--- app/src/main/java/com/yp_19102043/movieapp/activity/MainActivity2.kt
+++ app/src/main/java/com/yp_19102043/movieapp/activity/MainActivity2.kt
@@ -3,23 +3,29 @@ package com.yp_19102043.movieapp.activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.View
 import android.widget.Toast
+import androidx.core.view.isVisible
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import com.yp_19102043.movieapp.R
+import com.yp_19102043.movieapp.databinding.ActivityMain2Binding
 import com.yp_19102043.movieapp.databinding.ActivityMainBinding
 
 
-class MainActivity2 : AppCompatActivity() {
+class MainActivity2 : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var auth: FirebaseAuth
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMain2Binding
+    private lateinit var googleSignInClient: GoogleSignInClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMain2Binding.inflate(layoutInflater)
         setContentView(binding.root)
         auth = Firebase.auth
         val currentUser = auth.currentUser
@@ -37,6 +43,9 @@ class MainActivity2 : AppCompatActivity() {
             startActivity(intent)
             finish()
         }
+        else{
+            updateUI(currentUser)
+        }
         binding.btnSignOut.setOnClickListener(this)
         binding.btnEmailVerify.setOnClickListener(this)
         binding.btnfilmin.setOnClickListener(this)
@@ -51,7 +60,7 @@ class MainActivity2 : AppCompatActivity() {
                 sendEmailVerification()
             }
             R.id.btnfilmin -> {
-                sendEmailVerification()
+                filmin()
             }
         }
     }
@@ -89,6 +98,34 @@ class MainActivity2 : AppCompatActivity() {
             startActivity(intent)
             finish()
     }
+
+    private fun updateUI(currentUser: FirebaseUser) {
+        binding.btnEmailVerify.isVisible = false
+        currentUser.let {
+            val name = currentUser.displayName
+            val phoneNumber = currentUser.phoneNumber
+            val email = currentUser.email
+            val photoUrl = currentUser.photoUrl
+            val emailVerified = currentUser.isEmailVerified
+            val uid = currentUser.uid
+            binding.tvName.text = name
+            if(TextUtils.isEmpty(name)){
+                binding.tvName.text = "No Name"
+            }
+            binding.tvUserId.text = email
+            for (profile in it.providerData) {
+                val providerId = profile.providerId
+                if(providerId=="password" && emailVerified==false){
+                    binding.btnEmailVerify.isVisible = true
+                }
+                if(providerId=="phone"){
+                    binding.tvName.text = phoneNumber
+                    binding.tvUserId.text = providerId
+                }
+            }
+        }
+    }
+
 
 
 }
--- app/src/main/java/com/yp_19102043/movieapp/activity/SignInActivity.kt
+++ app/src/main/java/com/yp_19102043/movieapp/activity/SignInActivity.kt
@@ -6,7 +6,13 @@ import android.os.Bundle
 import android.text.TextUtils
 import android.view.View
 import android.widget.Toast
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import com.yp_19102043.movieapp.R
@@ -15,6 +21,8 @@ import com.yp_19102043.movieapp.databinding.ActivitySignInBinding
 class SignInActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var auth: FirebaseAuth
     private lateinit var binding: ActivitySignInBinding
+    private lateinit var googleSignInClient: GoogleSignInClient
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignInBinding.inflate(layoutInflater)
@@ -24,17 +32,40 @@ class SignInActivity : AppCompatActivity(), View.OnClickListener {
         binding.btnEmail.setOnClickListener(this)
         binding.btnPhone.setOnClickListener(this)
         auth = Firebase.auth
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
     }
+
     override fun onClick(v: View) {
         when (v.id) {
             R.id.btnSign -> {
-                signIn(binding.inputEmail.text.toString(),
-                    binding.inputPassword.text.toString())
+                signIn(
+                    binding.inputEmail.text.toString(),
+                    binding.inputPassword.text.toString()
+                )
             }
             R.id.tvSignUp -> {
                 val intent = Intent(this@SignInActivity, SignUpActivity::class.java)
                 startActivity(intent)
             }
+            R.id.btnEmail -> {
+                signIn()
+            }
+
+        }
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            val intent = Intent(this@SignInActivity, MainActivity2::class.java)
+            startActivityForResult(intent, 1)
+            finish()
         }
     }
 
@@ -46,12 +77,16 @@ class SignInActivity : AppCompatActivity(), View.OnClickListener {
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
                     finish()
-                    val intent = Intent(this@SignInActivity,
-                        MainActivity::class.java)
+                    val intent = Intent(
+                        this@SignInActivity,
+                        MainActivity2::class.java
+                    )
                     startActivity(intent)
                 } else {
-                    Toast.makeText(baseContext, "Authentication failed.",
-                        Toast.LENGTH_SHORT).show()
+                    Toast.makeText(
+                        baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT
+                    ).show()
                 }
             }
     }
@@ -75,4 +110,45 @@ class SignInActivity : AppCompatActivity(), View.OnClickListener {
         return valid
     }
 
+    private fun signIn() {
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, 9001)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 9001) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.getResult(ApiException::class.java)!!
+                firebaseAuthWithGoogle(account.idToken!!)
+            } catch (e: ApiException) {
+                val view = binding.mainLayout
+                Snackbar.make(
+                    view, "Google sign in failed.",
+                    Snackbar.LENGTH_SHORT
+                ).show()
+            }
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val user = auth.currentUser
+                    val view = binding.mainLayout
+                    Snackbar.make(view, "Authentication Success.",
+                        Snackbar.LENGTH_SHORT).show()
+                    val intent = Intent(this@SignInActivity, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                } else {
+                    val view = binding.mainLayout
+                    Snackbar.make(view, "Authentication Failed.",
+                        Snackbar.LENGTH_SHORT).show()
+                }
+            }
+    }
 }
